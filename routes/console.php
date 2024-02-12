08333d8372772a188f2d02e10ecf38d9a7bc5ef9
AreuShou <palouarlette3@gmail.com>
2024-02-12 18:35:01 +0000

    arlette commit2

--- routes/console.php
+++ routes/console.php
@@ -8,7 +8,7 @@ use Illuminate\Support\Facades\Artisan;
 | Console Routes
 |--------------------------------------------------------------------------
 |
-| This file is whe   kjre you may define all of your Closure based console
+| This file is whe   kjre you may definhhge all of your Closure based console
 | commands. Each Closure is bound to a command instance allowing a
 | simple approach to interacting with each command's IO methods.
 |
