c50feda64e02490133c7a13ee9abfbde9d8f9013
amourgobb <amourgobb@gmail.com>
2024-02-12 18:42:10 +0000

    lena commit

---
+++ app/Http/Controllers/TypeHebergementController.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\TypeHebergement;
+use App\Http\Requests\StoreTypeHebergementRequest;
+use App\Http\Requests\UpdateTypeHebergementRequest;
+
+class TypeHebergementController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(StoreTypeHebergementRequest $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(TypeHebergement $typeHebergement)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(TypeHebergement $typeHebergement)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(UpdateTypeHebergementRequest $request, TypeHebergement $typeHebergement)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(TypeHebergement $typeHebergement)
+    {
+        //
+    }
+}
